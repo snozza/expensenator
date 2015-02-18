804e9a1ebd187b7ed0988a55457abbaf935114fe
M <ming.chan@windowslive.com>
2015-02-18 17:06:38 +0000

    adds table and data to rows

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,18 +1,34 @@
 function alertMessage(message) {
   clearTimeout(this.timeout);
   $("#alert").text(message).show();
-  this.timeout = setTimeout(function() { $("#alert").hide(); }, 3000); 
+  this.timeout = setTimeout(function() { $("#alert").hide(); }, 3000);
 }
 
 $(document).ready(function() {
+
+  $('#expenses_table').hide();
+
   $('form').on('submit', function(event) {
     event.preventDefault();
+    console.log($(this).serialize());
     $.post('/', $('form').serialize(), function(data) {
       var data = JSON.parse(data);
-      $('#expenses_list').append('<li class="expense_item">Expense Type: ' + data['category'] +
-        ' Amount: ' + data['amount'] + ' Currency: ' + data['currency'] + ' Date: ' + data['day'] + ' '
-        + data['month'] + ' ' + data['year']);
+      // $('#expenses_list').append('<li class="expense_item">Expense Type: ' + data['category'] +
+      //   ' Amount: ' + data['amount'] + ' Currency: ' + data['currency'] + ' Date: ' + data['day'] + ' '
+      //   + data['month'] + ' ' + data['year']);
+      $('#expenses_table').append("<tr><td>" + data['project_name'] + "</td>" +
+      "</td>" + "<td>" + data['project_code'] + "</td>" +
+      "</td>" + "<td>" + data['amount'] + "</td>" +
+      "</td>" + "<td>" + data['currency'] + "</td>" +
+      "</td>" + "<td>" + data['day'] + " " + data['month'] + " " + data['year'] + "</td>" +
+      "</td>" + "<td>" + data['category'] + "</td>" +
+      "</td>" + "<td>" + data['description'] + "</td>" +
+      "</td>" + "<td>" + data['city'] + "</td>" +
+      "</td>" + "<td>" + data['country'] + "</td>" +
+      "</tr>");
     });
+
+    $('#expenses_table').show();    
   });
 
 $('#project_name').change(function() {
