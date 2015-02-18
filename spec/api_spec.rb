f5b05c44ec8df23f3802fd7e192704f3d9cf525d
Andrew Snead <andrewcsnead@gmail.com>
2015-02-18 13:43:16 +0000

    add simple form

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -21,5 +21,11 @@ describe 'Server Routes' do
       expect(last_response).to be_ok
     end
 
+    xit 'a post request has parameters' do
+      post '/', {:category => 'taxi', :amount => '1'}.to_json, "CONTENT_TYPE" => "application/json"
+      p last_response
+      expect(last_response.body).to eq({"category" => 'taxi', "amount" => '1'})
+    end
+
   end
 end
---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+feature 'Homepage' do
+
+  before(:each) do
+    visit '/'
+  end
+
+  scenario 'has a title' do
+    expect(page).to have_title "Expensenator"
+  end
+
+  scenario 'has a form to submit an expense' do
+    expect(page).to have_selector('form')
+  end
+end
+
+feature 'Submiting a new expense' do
+
+  before(:each) do
+    visit '/'
+  end
+
+  scenario 'request summary appears on page after submission' do 
+    fill_in "employee_id", with: "1"
+    fill_in "amount", with: "1000"
+    select("taxi", from: "category")
+    fill_in "description", with: "Some Great Food"
+    click_button 'submit'
+    expect(find('.expense_item').text).to include? '£1000'
+  end
+
+end
